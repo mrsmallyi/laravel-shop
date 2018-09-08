e054c0519597090041b028f8d5bf64b2234b25f9
hongjinghong <hongjinghong@myzaker.com>
2018-09-08 22:18:35 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">
+			由<a href="https://github.com/mrsmallyi" target="_blank">mrsmallyi</a> 编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+		</p>
+		<p class="pull-right"><a href="maillto:hjh1170459853@qq.com">联系本人</a></p>
+	</div>
+</footer>
